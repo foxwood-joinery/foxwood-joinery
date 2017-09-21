0b5ee5e22439141b67a5c2352ca3736bdb103de7
Ian Parr <ianjudparr@gmail.com>
2017-09-21 19:06:08 +0100

    initial

--- deploy.ps1
+++ deploy.ps1
@@ -5,10 +5,11 @@ git add -A
 git commit -am $message
 git push origin master
 
- hugo -d ../[CHANGEME].github.io
+ hugo -d ../foxwood-joinery.github.io
 
 cd ..
-cd [CHANGEME].github.io
+cd foxwood-joinery
+.github.io
 
 # Commit changes.
 git add -A
