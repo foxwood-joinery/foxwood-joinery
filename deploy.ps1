4e36bd65254e01fb7eb41a1d9b619bdf1a35bbf2
Ian Parr <ianjudparr@gmail.com>
2017-09-21 19:59:14 +0100

    init

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,22 @@
 $message = $args[0]
 
+$siteName = "foxwood-joinery"
+
 # Commit changes.
 git add -A
 git commit -am $message
 git push origin master
 
- hugo -d ../foxwood-joinery.github.io
+ hugo -d "../$siteName.github.io"
 
 cd ..
-cd foxwood-joinery.github.io
+cd "$siteName.github.io"
 
 # Commit changes.
+git init
+
+git remote add origin "https://github.com/$siteName/$siteName.github.io"
+
 git add -A
 git commit -am $message
 git push origin master
